195cf13905fb470158d476b8b2a8fabeaf5d565e
lautaroilla <lautaro12387@gmail.com>
2023-01-08 22:54:14 +0000

    Fix

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,5 +1,6 @@
 sudo apt-get update
 wget https://github.com/gohugoio/hugo/releases/download/v0.57.2/hugo_0.57.2_Linux-64bit.deb
 sudo dpkg -i hugo_0.57.2_Linux-64bit.deb
+apt-get install hugo
 apt-get install make
 make build
